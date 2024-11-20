8c2437a038f34eddb189579a242b8ee78e8decdd
toketime <flatoke@gmail.com>
2024-11-21 03:16:43 +0900

    imgage fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,4 @@
-// もともと入っていたコード
-// import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
