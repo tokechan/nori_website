27bc4f39d0bdbdfa967521115df66b5f43df9898
toketime <flatoke@gmail.com>
2024-11-20 23:51:56 +0900

    create some page

---
+++ data/projects.ts
@@ -0,0 +1,59 @@
+export interface Project {
+    id: number;
+    title: string;
+    image: string;
+    link: string;
+    // description: string;
+    // data?: string;
+}
+
+export const projects: Project[] = [
+    {
+        id: 1,
+        title: "ひつじサミット尾州",
+        image: "/images/hitsuji.jpeg",
+        link: "https://hitsuji.fun/",
+      },
+      {
+        id: 2,
+        title: "WESAY",
+        image: "/images/wesay.jpeg",
+        link: "https://wesay.hearst.co.jp/",
+      },
+      {
+        id: 3,
+        title: "宇野亞喜良展 AQUIRAX UNO",
+        image: "/images/uno.jpeg",
+        link: "https://www.tokyoartbeat.com/articles/-/aquirax-uno-2024-news-202401",
+      },
+      {
+        id: 4,
+        title: "TOKYO ART BEAT 20th AWARD",
+        image: "/images/artbeat1.jpeg",
+        link: "https://www.tokyoartbeat.com/articles/-/tokyo-art-beat-20th-award-news-202408",
+      },
+      {
+        id: 5,
+        title: "Daily work",
+        image: "/images/norichan2.jpg",
+        link: "https://www.instagram.com/unlabeling___/",
+      },
+      {
+        id: 6,
+        title: "Demo image3",
+        image: "/images/hiratsuka1.jpg",
+        link: "https://hitsuji.fun/",
+      },
+      {
+        id: 7,
+        title: "Demo image4",
+        image: "/images/hiratsuka1.jpg",
+        link: "https://hitsuji.fun/",
+      },
+      {
+        id: 8,
+        title: "Demo image5",
+        image: "/images/hiratsuka1.jpg",
+        link: "https://hitsuji.fun/",
+      },
+]
