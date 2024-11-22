d82bfdb54b5ed5fb5bbc829e8f4240c637bebaaf
toketime <flatoke@gmail.com>
2024-11-22 23:41:12 +0900

    box delete

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TextField, Button, Box, Typography, styled } from '@mui/material';
+import { TextField, Button, Typography, styled } from '@mui/material';
 import { useState } from 'react';
 
 const StyledTextField = styled(TextField)({
