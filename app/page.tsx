e15ce85153fa94d80182e2e05a45a6067dbfaa64
toketime <flatoke@gmail.com>
2025-01-08 23:08:37 +0900

    delete no need file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { projects } from '../data/projects'
 import HeroCircle from '../components/ui/hero-circle'
-import VideoBackground from '../components/VideoBackground'
 
 
 
