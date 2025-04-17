c92409cc414bdf4c5c2db2edcc47711dad3f8308
tokechan <fleatoke@gmail.com>
2025-04-17 23:55:27 +0900

    fix: restartgit add .

--- utils/youtube.ts
+++
@@ -1,8 +0,0 @@
-export function getYoutubeId(url: string): string {
-  const match = url.match(/[?&]v=([^&]+)/);
-  return match ? match[1] : '';
-}
-
-export function getYoutubeThumbnailUrl(videoId: string): string {
-  return `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`
-}
